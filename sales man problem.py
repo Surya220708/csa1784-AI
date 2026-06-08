0cf4cc080a0cbb9b5a832cc4e5624baa6ea22ef0
Surya220708 <saisuryatej2025@gmail.com>
2026-06-08 10:05:02 +0530

    Add files via upload

---
+++ sales man problem.py
@@ -0,0 +1,31 @@
+from itertools import permutations
+
+graph = [
+    [0, 10, 15, 20],
+    [10, 0, 35, 25],
+    [15, 35, 0, 30],
+    [20, 25, 30, 0]
+]
+
+n = len(graph)
+cities = list(range(n))
+
+min_cost = float('inf')
+best_path = None
+
+for path in permutations(cities[1:]):
+    cost = 0
+    current = 0
+
+    for city in path:
+        cost += graph[current][city]
+        current = city
+
+    cost += graph[current][0]
+
+    if cost < min_cost:
+        min_cost = cost
+        best_path = (0,) + path + (0,)
+
+print("Shortest Path:", best_path)
+print("Minimum Cost:", min_cost)
